cff8fad5eb55b6e31dd6c01c1148cbdc81468046
JhonWick9191 <singhaditya5329@gmail.com>
2025-12-09 16:13:09 +0530

    adding

--- Frontend/src/Components/NavBar.jsx
+++ Frontend/src/Components/NavBar.jsx
@@ -188,7 +188,7 @@ export default function NavBar() {
             </li>
 
             <li className="profile-logo-in-list">
-              {User? (
+              {User && User.image ?  (
                 <img
                   onClick={() => Navigate("/profile")}
                   src={User.image || "https://via.placeholder.com/40"}
