04aa381c5ec6947e323cfbd55ec98e95e4bbf184
JhonWick9191 <singhaditya5329@gmail.com>
2026-02-05 13:13:12 +0530

    adding code for main poster section

--- Frontend/src/Components/MainPoster.jsx
+++ Frontend/src/Components/MainPoster.jsx
@@ -10,6 +10,28 @@ import { GrNext } from "react-icons/gr";
 
 
 const DesktopPosterImage = [
+    {
+    product_id: 999,
+    _id: "hardcoded-1000",
+    Brand_Name: "Akai",
+    Model_number: "MPC XL",
+    Product_Category: "MPC",
+    Product_Discripction: "MPC XL has everything you need for pro-level music making. It features ultra-responsive MPCe pads, a dedicated step sequencer, touch-sensitive Q-Links with OLED displays, a next-gen 8-core processor, 16 GB RAM, hands-on performance controls, advanced sequencing tools, deep I/O connectivity, and so much more. Integrated with MPC3 OS, this system supports up to 32 plugin instruments, 16 audio tracks, and 256 voices, making it a studio-grade workhorse",
+    Product_Name: "MPC XL",
+    Product_Quantity: 5,
+    Product_price: 222956,
+
+    Product_Type: "Guitars",
+    VenderTex_Rate: 0.18,
+    Vendor_price: 345582,
+    new: "yes",
+    __v: 0,
+    banner_image:"https://pub-d5d786d675024a039884449faea17b9e.r2.dev/Akai%20MPC%20XL.png",
+    image_01: "https://cdn.builder.io/api/v1/image/assets%2F47c5d023e7b3451a86ac03602c65dce8%2F377e222fc51e4f9c9f1ed80d56639956?format=webp&width=2000",
+    image_02: "https://cdn.builder.io/api/v1/image/assets%2F47c5d023e7b3451a86ac03602c65dce8%2Fae140207c61642489f61d7b7132a916d?format=webp&width=2000",
+    image_03: "https://cdn.builder.io/api/v1/image/assets%2F47c5d023e7b3451a86ac03602c65dce8%2F1e709e45a3704e64a09e2550991f7203?format=webp&width=2000",
+   
+  },
   {
     product_id: 1000,
     _id: "hardcoded-1000",
@@ -34,11 +56,11 @@ const DesktopPosterImage = [
   {
     product_id: 1001,
     _id: "hardcoded-1001",
-    Brand_Name: "Fender",
-    Model_number: "0145623010",
-    Product_Category: "Electric Guitar",
-    Product_Discripction: "Player Stratocaster delivers classic Strat tone with modern playability. Alder body, maple neck, and SSS pickup configuration for versatile Fender sound.",
-    Product_Name: "Player Stratocaster PF",
+    Brand_Name: "Alesis",
+    Model_number: "Nitro Amp Pro",
+    Product_Category: "Amplifiers",
+    Product_Discripction:"The Alesis Nitro Amp Pro is a compact drum speaker, featuring 80 watts of drum-amplifying power, an 8” woofer that delivers accurate low-end response, a 2.5” tweeter for brilliant high-end, and Bluetooth Audio streaming with separate volume control all within one floor-wedge design. ",
+    Product_Name: "Nitro Amp Pro",
     Product_Quantity: 8,
     Product_price: 18500,
     Product_Type: "Guitars",
